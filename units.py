0e92356ec6156f666f488307060e5ee4617a0601
Tornike-I <tornike.kad@gmail.com>
2026-02-21 16:33:16 +0100

    added location parameter to zombie class

--- units.py
+++ units.py
@@ -1,18 +1,18 @@
 class Zombie:
-    def __init__(self):
-        self.location = 1
+    def __init__(self, location):
+        self.location = location
 
 class NormalZombie(Zombie):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, location):
+        super().__init__(location)
         self.hp = 1
 
 class FatZombie(Zombie):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, location):
+        super().__init__(location)
         self.hp = 2
 
-class veryFatZombie(Zombie):
-    def __init__(self):
-        super().__init__()
+class VeryFatZombie(Zombie):
+    def __init__(self, location):
+        super().__init__(location)
         self.hp = 3
